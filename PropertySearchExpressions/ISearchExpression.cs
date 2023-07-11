72c099b4e55f146246eae4d07cd1abed0c705bc4
C. J. Anderson <chris@standsure.io>
2023-07-11 09:53:01 -0400

    add ISearchExpression

---
+++ PropertySearchExpressions/ISearchExpression.cs
@@ -0,0 +1,25 @@
+#nullable enable
+
+namespace Taazaa.Shared.DevKit.Framework.Search.PropertySearchExpressions;
+
+using System.Linq.Expressions;
+
+using JetBrains.Annotations;
+
+/// <summary>
+///     Defines a search expression.
+/// </summary>
+[PublicAPI]
+public interface ISearchExpression
+{
+    /// <summary>
+    ///     Gets the expression.
+    /// </summary>
+    /// <param name="memberExpression">
+    ///     The member expression.
+    /// </param>
+    /// <returns>
+    ///     The <see cref="Expression" />.
+    /// </returns>
+    public Expression GetExpression(MemberExpression memberExpression);
+}
