dbd3705b66498c060afa50fdc75d8172c5165c61
C. J. Anderson <christopher.anderson@taazaa.com>
2024-04-02 15:10:11 -0400

    docs, unit

--- makedocs.sh
+++ makedocs.sh
@@ -3,4 +3,5 @@
 # install if missing
 dotnet tool list -g | (grep xmldocmd || true) | wc -l | xargs test 0 -eq && dotnet tool install xmldocmd -g
 
-xmldocmd ./bin/Debug/netstandard2.0/Taazaa.Shared.DevKit.Framework.Search.dll docs/
+rm -rf docs/
+~/repos/XmlDocMarkdown/src/xmldocmd/bin/Debug/net8.0/xmldocmd ./bin/Debug/netstandard2.0/Taazaa.Shared.DevKit.Framework.Search.dll docs/
