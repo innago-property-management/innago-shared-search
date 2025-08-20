1ff3d6ecdb55133d494dd66d836fbdb0abd6b71f
chris-innago <chris@innago.com>
2025-08-20 14:41:53 +0530

    cleanup

--- Properties/InternalsVisibleTo.cs
+++
@@ -1,4 +0,0 @@
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("UnitTests")]
-[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
--- makedocs.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env bash
-
-# install if missing
-dotnet tool list -g | (grep xmldocmd || true) | wc -l | xargs test 0 -eq && dotnet tool install xmldocmd -g
-
-rm -rf docs/
-~/repos/XmlDocMarkdown/src/xmldocmd/bin/Debug/net8.0/xmldocmd ./bin/Debug/netstandard2.0/Taazaa.Shared.DevKit.Framework.Search.dll docs/
